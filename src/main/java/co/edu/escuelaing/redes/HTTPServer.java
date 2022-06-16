6f41b069a69bd7f0c866e6cb0d546fc9a56daa71
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-06-16 12:10:05 -0500

    Añadido GetPort

--- src/main/java/co/edu/escuelaing/redes/HTTPServer.java
+++ src/main/java/co/edu/escuelaing/redes/HTTPServer.java
@@ -24,7 +24,7 @@ public class HTTPServer {
         while(run){
             ServerSocket serverSocket = null;
             try {
-                serverSocket = new ServerSocket(35000);
+                serverSocket = new ServerSocket(_instance.getPort());
             } catch (IOException e) {
                 System.err.println("Could not listen on port: 35000.");
                 System.exit(1);
